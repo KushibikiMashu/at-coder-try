730f41f06544e034c9946bec2dea68102d15f485
KushibikiMashu <kushibiki3413@gmail.com>
2020-03-05 13:54:50 +0900

    UnionFindをclass化

--- weapons/union_find.py
+++ weapons/union_find.py
@@ -1,3 +1,36 @@
+class UnionFind:
+    parent = []
+    rank = []
+
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        else:
+            self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
+
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y: return
+
+        if self.rank[x] < self.rank[y]:
+            self.parent[x] = y
+        else:
+            self.parent[y] = x
+            if self.rank[x] == self.rank[y]:
+                self.rank[x] += 1
+                return self.rank[x]
+
+    def isSame(self, x, y):
+        return self.find(x) == self.find(y)
+
+uf = UnionFind(10)
+
 # parant, rankの初期化
 n = 10
 parent = [i for i in range(n)]
@@ -40,4 +73,3 @@ def unite(x, y, parent):
         if rank[x] == rank[y]:
             rank[x] += 1
             return rank[x]
-
