6e1bcc3c354c929b8743d9f284f98cd7a780b31e
KushibikiMashu <kushibiki3413@gmail.com>
2020-03-08 17:03:33 +0900

    ダイクストラ法を追加

--- weapons/最短経路.py
+++ weapons/最短経路.py
@@ -48,9 +48,10 @@ def solve():
                     d[t] = d[f] + c
                     update = True
                 # 無向グラフなので、from toを入れ替える
-                if d[t] != INF and d[f] > d[t] + c:
-                    d[f] = d[t] + c
-                    update = True
+                # TODO これ本当に必要？
+#                 if d[t] != INF and d[f] > d[t] + c:
+#                     d[f] = d[t] + c
+#                     update = True
 
             if not update: break
         return d
@@ -71,6 +72,43 @@ print(solve()) # 16
 # https://juppy.hatenablog.com/entry/2018/11/01/%E8%9F%BB%E6%9C%AC_python_%E5%8D%98%E4%B8%80%E6%9C%80%E7%9F%AD%E7%B5%8C%E8%B7%AF%E6%B3%952%EF%BC%88%E3%83%80%E3%82%A4%E3%82%AF%E3%82%B9%E3%83%88%E3%83%A9%E6%B3%95%EF%BC%89_%E7%AB%B6%E6%8A%80%E3%83%97
 
 # https://juppy.hatenablog.com/entry/2019/02/18/%E8%9F%BB%E6%9C%AC_python_%E3%83%97%E3%83%A9%E3%82%A4%E3%82%AA%E3%83%AA%E3%83%86%E3%82%A3%E3%82%AD%E3%83%A5%E3%83%BC%28heapq%29%E3%82%92%E7%94%A8%E3%81%84%E3%81%9F%E3%83%97%E3%83%AA%E3%83%A0%E6%B3%95
+
+from collections import deque, defaultdict
+
+def solve():
+    V, E = map(int, input().split())
+    G = [[] for _ in range(V)]
+    for _ in range(E):
+        a, b, c = map(int, input().split())
+        # 隣接リスト表現
+        G[a-1].append([b-1, c])
+
+    INF = 10 ** 5
+    d = [INF for _ in range(V)]
+
+    # sがstartの頂点
+    def dijkstra(s):
+        # スタート地点のコストを0にする
+        d[s] = 0
+        # (頂点, 最短距離)
+        q = deque([(s, 0)])
+
+        while q:
+            p = q.popleft()
+            v = p[0]
+            if d[v] < p[1]: continue
+            for i in range(len(G[v])):
+                to, cost = G[v][i]
+                if d[to] > d[v] + cost:
+                    # 最小コストを更新する
+                    d[to] = d[v] + cost
+                    q.append((to, d[to]))
+        return d
+
+    ans = dijkstra(0)
+    return ans[6]
+
+print(solve()) # 17
 
 # ---
 # ワーシャル・フロイド法
