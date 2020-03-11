325711b419b7ff624c02902b0278649d897ef8db
KushibikiMashu <kushibiki3413@gmail.com>
2020-03-11 09:40:08 +0900

    unit_test用のテンプレートを追加

---
+++ weapons/unit_test.py
@@ -0,0 +1,23 @@
+def solve():
+    # N = int(input())
+    # N, M = map(int, input().split())
+    # A = list(map(int, input().split()))
+    # A = [int(input()) for _ in range(N)]]
+    # for _ in range(N):
+    #     a, b = list(map(int, input().split()))
+    # 二次元配列
+    # INF = 10 ** 9
+    # d = [[INF] * (M + 100) for _ in range(N)]
+
+    return 0
+
+# print(solve())
+
+# def solve()が@return voidのとき
+# solve()
+
+# テスト用
+def resolve():
+    print(solve())
+
+
