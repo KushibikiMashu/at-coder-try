2a91423a76e59ff89ebaa2f08691ef30428b76e2
KushibikiMashu <kushibiki3413@gmail.com>
2020-03-11 09:40:35 +0900

    テンプレートを削除

--- weapons/template.py
+++ weapons/template.py
@@ -7,32 +7,6 @@
 # ゼロ埋め
 # l = [0] * N
 
-def solve():
-    # N = int(input())
-    # N, M = map(int, input().split())
-    # A = list(map(int, input().split()))
-    # A = [int(input()) for _ in range(N)]]
-    # for _ in range(N):
-    #     a, b = list(map(int, input().split()))
-    # 二次元配列
-    # INF = 10 ** 9
-    # d = [[INF] * (M + 100) for _ in range(N)]
-
-    return 0
-
-# print(solve())
-
-# def solve()が@return voidのとき
-# solve()
-
-# テスト用
-def resolve():
-    print(solve())
-
-
-# -----
-
-
 # MOD = 10 ** 9 + 7
 
 def solve():
